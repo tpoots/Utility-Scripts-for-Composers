deb79834639b567f2b6474bf532d3734f1fa05dd
Agoston Petz <apetz@salesforce.com>
2021-09-05 12:08:03 -0500

    Renaming and ordering instruments by config

--- ExpressionMaps/expression-map-parser.py
+++ ExpressionMaps/expression-map-parser.py
@@ -7,6 +7,24 @@ from os import walk
 
 # https://htmlcolorcodes.com/
 
+artMapConfig = {
+    'SFSW Piccolo Flute CB D.expressionmap': {'name': 'Piccolo Flute', 'order': 0},
+    'SFSW Flute solo CB D.expressionmap': {'name': 'Flute Solo', 'order': 1},
+    'SFSW Flute A2 CB D.expressionmap': {'name': 'Flutes a2', 'order': 2},
+    'SFSW Alto Flute CB D.expressionmap': {'name': 'Alto Flute', 'order': 3},
+    'SFSW Bass Flute CB D.expressionmap': {'name': 'Bass Flute', 'order': 4},
+    'SFSW Oboe Solo CB D.expressionmap': {'name': 'Oboe Solo', 'order': 5},
+    'SFSW Oboes A2 CB D.expressionmap': {'name': 'Oboes a2', 'order': 6},
+    'SFSW Cor Anglais CB D.expressionmap': {'name': 'Cor Anglais', 'order': 7},
+    'SFSW Clarinet Solo CB D.expressionmap': {'name': 'Clariet Solo', 'order': 8},
+    'SFSW Clarinet A2 CB D.expressionmap': {'name': 'Clarinets a2', 'order': 9},
+    'SFSW Bass Clarinet CB D.expressionmap': {'name': 'Bas Clarinet', 'order': 10},
+    'SFSW Contrabass Clarinet CB D.expressionmap': {'name': 'Contrabass Clarinet', 'order': 11},
+    'SFSW Bassoon Solo CB D.expressionmap': {'name': 'Bassoon Solo', 'order': 12},
+    'SFSW Bassoon A2 CB D.expressionmap': {'name': 'Bassoons a2', 'order': 13},
+    'SFSW ContraBassoon CB D.expressionmap': {'name': 'ContraBassoon', 'order': 14}
+}
+
 allExpressionMaps = []
 for (dirpath, dirnames, filenames) in walk(sys.argv[1]):
     for file in filenames:
@@ -19,7 +37,7 @@ for expressionMap in allExpressionMaps:
     # print(f.read())
     # print expressionMap
 
-    m = re.search('.*\/(.*).expressionmap', expressionMap)
+    m = re.search('.*\/(.*)', expressionMap)
     instrumentName = m.group(1)
 
     tree = ET.parse(expressionMap)
@@ -46,9 +64,13 @@ for expressionMap in allExpressionMaps:
             # print('------------')
             # CSV output - print(instrumentName + ',' + articulation + ',' + keySwitch + ',' + uacc)
             if (instrumentName.find('CB D') >= 0):
-                if(not instrumentName in articulationMap.keys()):
-                    articulationMap[instrumentName] = {}
-                articulationMap[instrumentName].update({articulation: {'keySwitch': keySwitch, 'UACC': uacc}})
+                config = artMapConfig.get(instrumentName)
+                instrumentIndex = config.get("order")
+                instrumentName = config.get("name")
+                if (not instrumentIndex in articulationMap.keys()):
+                    articulationMap[instrumentIndex] = {}
+                    articulationMap[instrumentIndex].update({"name": instrumentName, "articulations": {}})
+                articulationMap[instrumentIndex]["articulations"].update({articulation: {'keySwitch': keySwitch, 'UACC': uacc}})
 
     #for element in tree.iter():
     #    if (element.get('class') == "PSoundSlot"): # found an articulation
