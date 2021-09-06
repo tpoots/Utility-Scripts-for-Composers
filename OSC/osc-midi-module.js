d9e5c7a3908eaf03cbc554921a5ce375d90e539b
Toony Poots <tpootsmusic@gmail.com>
2021-09-06 14:26:18 -0500

    Cleaned up debug messaging

--- OSC/osc-midi-module.js
+++ OSC/osc-midi-module.js
@@ -8,8 +8,6 @@ module.exports = {
 
         // Filter incoming osc messages
         var {address, args, host, port} = data
-        console.log("starting processing")
-        console.log("host: " + host  + " port: " + port + " address: " + address + " args[0]: " + args[0].value + " args[1]: " + args[1].value + " args[2]: " + args[2].value)
 
         // return a C-2 poly pressure message if receiving a track select (CC 126 on channel 7)
         if (port == 'ControlToOSC' && address == '/control' && args[0].value == 7 && args[1].value == 126 && args[2].value === 100) {
@@ -22,6 +20,7 @@ module.exports = {
             var note = args[1].value
             var pressure = args[2].value
             var buttonId = "button_instr_" + note
+            // use channel information to select the correct instruments
             if (channel == 1) {
                 // woodwinds section
                 receive('/SET', 'section_selector_script', 'ssw')
@@ -29,17 +28,9 @@ module.exports = {
                 // brass section
                 receive('/SET', 'section_selector_script', 'ssb')
             }
-            console.log("PP " + note + " on channel " + channel)
             receive('/SET', buttonId, 1)
             return
         }
-
-        // if (address === '/control' && args[0].value === 7 && args[1].value === 126 && args[2].value === 100) {
-        //    // received a track selector message so probe for instrument information
-        //    send(host, port, '/key_pressure', 1, 127, 101)
-        //    console.log("sent poly pressure 127")
-        //    return
-        // }
 
         return {address, args, host, port}
 
