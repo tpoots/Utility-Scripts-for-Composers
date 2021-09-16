18eef38e22c6072a2435780f93c4677dee8cb391
Toony Poots <tpootsmusic@gmail.com>
2021-09-16 12:59:36 -0500

    Added generate-osc-json-configuration.py

--- ExpressionMaps/generate-csv-configs.py
+++ ExpressionMaps/generate-csv-configs.py
@@ -11,18 +11,28 @@ from os import walk
 #   python generate-csv-configs.py {directory-containing-expression-maps |individual-expression-map}
 #
 # This script parses directories (or individual files) containing Cubase
-# expression maps and generates two CSV output files:
+# expression maps and generates three CSV output files:
 #
-# 1. map-config.csv - a file listing every unique combination of expression
-#    map and articulation, with keyswitch trigger and UACC value
+# 1. all-expression-maps-and-articulations.csv - a file listing every unique
+#    combination of expression map and articulation, with keyswitch trigger and
+#    UACC value (UACC defaults to 127 if not available)
 #
-# 2. articulations.csv - a file containing just the unique articulations present
-#    in the expression maps, sorted by the frequency in which they occurred
+# 2. library-config.csv - a file containing just the unique expression map
+#    names, allowing the user to define a custom name and order in which the
+#    instrument should appear in the UI (by filling in the second and third
+#    columns).
 #
-# if the output files already exist THEY WILL BE OVERWRITTEN
+# 3. articulations.csv - a file containing just the unique articulations present
+#    in the expression maps, sorted by the frequency in which they occurred.
+#    This file allows the user to define the location each articulation
+#    should occupy in the OSC controller UI, by filling in the third column.
+#    This step is OPTIONAL and only required if you are using "library"
+#    style articulation buttons
 #
-# These files are useful for analyzing the contents of a set of Cubase Expression
-# maps. However their main purpose is to be provided as input to the subsequent
+# If the output files already exist THEY WILL BE OVERWRITTEN
+#
+# The first file is useful for analyzing the contents of a set of Cubase Expression
+# maps. The purpose of the second two are to be provided as input to the subsequent
 # script (generate-osc-json-configuration.py) which takes the expression maps
 # plus the user-provided map-config.csv and articulations.csv and generates the
 # json configuration needed by the OSC cubase controller.
@@ -45,11 +55,15 @@ if os.path.isdir(expressionMapsPath):
 else:
     allExpressionMaps.append(expressionMapsPath)
 
-mapConfig = open('map-config.csv', 'w')
-mapConfig.write("Library Name (needs user input), ExpressionMapName, Articulation, Keyswitch, UACC\n")
+allMapsAnArts = open('all-expression-maps-and-articulations.csv', 'w')
+allMapsAnArts.write("ExpressionMapName, Articulation, Keyswitch, UACC\n")
 
 articulationsConfig = open('articulations.csv', 'w')
-articulationsConfig.write('UI Button Location (needs user input), Articulation, Frequency')
+articulationsConfig.write('Articulation, Frequency, UI Button Location (needs user input)\n')
+
+mapConfig = open('map-config.csv', 'w')
+mapConfig.write('ExpressionMapName, Name (needs user input), Order (needs user input)\n')
+
 
 pathRegex = ""
 if (platform.system() == 'Windows'):
@@ -62,6 +76,8 @@ for expressionMap in allExpressionMaps:
 
     m = re.search(pathRegex, expressionMap)
     instrumentArtMapName = m.group(1)
+
+    mapConfig.write(instrumentArtMapName + ', , \n')
 
     tree = ET.parse(expressionMap)
     root = tree.getroot()
@@ -81,8 +97,8 @@ for expressionMap in allExpressionMaps:
             if (articulationElement.find(".//obj[@class='PSlotMidiAction']/member[@name='midiMessages']//obj[@class='POutputEvent']/int[@name='data2']") is not None):
                 uacc = articulationElement.find(".//obj[@class='PSlotMidiAction']/member[@name='midiMessages']//obj[@class='POutputEvent']/int[@name='data2']").get('value')
 
-            mapConfig.write(' ,' + instrumentArtMapName + ',' + str(articulation) + ',' + str(keySwitch) + ',' + str(uacc) + '\n')
+            allMapsAnArts.write(instrumentArtMapName + ',' + str(articulation) + ',' + str(keySwitch) + ',' + str(uacc) + '\n')
 
 # create articulations.csv by sorting the frequency of all the unique articulations we have parsed
 for art in sorted(allArticulations.items(), key=lambda item: item[1], reverse=True):
-    articulationsConfig.write(' ,' + art[0] + ',' + str(art[1]) + '\n')
+    articulationsConfig.write(art[0] + ',' + str(art[1]) + ' , \n')
