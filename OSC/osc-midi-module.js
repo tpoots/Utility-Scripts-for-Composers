f8a87e4ad2d95ab249daf33c0d13f1bd8b4fbf9f
Toony Poots <tpootsmusic@gmail.com>
2022-08-13 16:30:31 -0500

    Minor tweaks, added lookup table to map incoming MIDI select data to library position.

--- OSC/osc-midi-module.js
+++ OSC/osc-midi-module.js
@@ -1,5 +1,36 @@
 var clients = []
-
+var libraryPositionMap = {
+1: 1, // ssw
+2: 2, // ssb
+3: 3, // sss
+4: 4, // scs
+5: 5, // sst
+6: 27, // bbcso woods
+7: 28, // bbcso brass
+8: 29, // bbcso strings
+9: 30, // bbcso perc
+10: 8, // sa1
+11: 9, // sa7
+12: 7, // ar1
+13: 22, // ewc
+14: 6, // sas
+15: 26, // bht
+16: 23, // sa4 winds
+17: 24, // sa4 brass
+18: 25, // sa4 strings
+19: 10, // sos
+20: 11, // sps
+21: 12, // oace
+22: 13, // bdt
+23: 14, // cdt
+24: 24, //
+25: 25, //
+26: 26, //
+27: 27, //
+28: 28, //
+29: 29, //
+30: 30  //
+}
 
 app.on('open', (data, client) => {
     console.log("Client connected...")
@@ -36,7 +67,8 @@ module.exports = {
             var buttonId = "button_instr_" + note
             // use channel information to select the correct instruments
             console.log("about to set library_selector_script")
-            receive('/SET', 'library_selector_script', channel)
+            libraryPosition = libraryPositionMap[channel]
+            receive('/SET', 'library_selector_script', libraryPosition)
             // select the button corresponding to the library
             console.log("about to (pre)set button " + buttonId)
             receive('/SET', buttonId, 1)
@@ -46,7 +78,8 @@ module.exports = {
             var library = args[2].value
             console.log("Library value = " + library)
             console.log("about to set library_selector_script")
-            receive('/SET', 'library_selector_script', library)
+            libraryPosition = libraryPositionMap[library]
+            receive('/SET', 'library_selector_script', libraryPosition)
             return
         } else if (port == 'ControlToOSC' && address == '/control' && args[0].value == 9 && args[1].value == 127 && args[2].value != 0) {
             // Instrument track selected in Cubase - set corresponding instrument
